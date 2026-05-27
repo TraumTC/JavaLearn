374a107ab6a41b12c4036544e3db21949861dc60
TraumTC <tc_hj23@163.com>
2026-05-27 22:37:45 +0800

    提交

---
+++ src/LeetCode/day10/LC150_11.java
@@ -0,0 +1,16 @@
+package LeetCode.day10;
+
+import java.util.Arrays;
+
+public class LC150_11 {
+    public static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = 0, i = citations.length - 1;
+        while (i >= 0 && citations[i] > h) {
+            h++;
+            i--;
+        }
+        return h;
+    }
+
+}
